a69da957b43456eeccc016be9e79398b666005db
Filipe Sartori <71408712+filipesartori@users.noreply.github.com>
2021-05-03 15:57:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
-from cx_Freeze import setup, Executable
-import os
-import cx_Freeze
-os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python36\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Program Files\Python36\tcl\tcl8.6'
+import tkinter as tk
+import uuid
+import webbrowser
 
-executables=[cx_Freeze.Executable('interface.py')]
+menu_inicial = tk.Tk()
+menu_inicial.title("By:FilipePagodeiro")
+menu_inicial.geometry('300x150')
+menu_inicial.resizable(False, False)
+menu_inicial.iconbitmap("imgs/icon2.ico")
 
-cx_Freeze.setup(name='CeasarCypher',version='0.1',description='foda',
-      options={'build_exe':{'packages':['tkinter']}},
-      executables=executables,
-      )
+def senha_aleatoria(string_length=10):
+    """Retorna um string aleatoria em string_length."""
+    random = str(uuid.uuid4())  
+    random = random.replace("-","") 
+    return random[0:string_length] 
+
+def abrir_img():
+    site = 'https://prnt.sc/' + senha_aleatoria(6)
+    abrir_site = webbrowser.open(site, new=0, autoraise=True)
+    return abrir_site
+
+cmd = tk.Button(menu_inicial, text='Clique aqui para abrir uma imagem aleatoria', command=abrir_img, bg='black', fg='#00FF00', width= 150, height= 75)
+cmd.pack()
+
+menu_inicial.mainloop()
+
+input("Pressione enter")
